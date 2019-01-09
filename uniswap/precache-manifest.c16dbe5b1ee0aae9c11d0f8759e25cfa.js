512e4ce570adee17f8b137e3e2014beb610b1144
Derrick Farris <mr.dcfarris@gmail.com>
2019-01-09 16:06:40 -0600

    Remove Bancor widget, add Uniswap widget

---
+++ uniswap/precache-manifest.c16dbe5b1ee0aae9c11d0f8759e25cfa.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb563c38afade37f55d04f2cd853d167",
+    "url": "./static/media/brave-logo.cb563c38.png"
+  },
+  {
+    "revision": "faebadbf95c3c3085587",
+    "url": "./static/css/main.cc2de7f6.chunk.css"
+  },
+  {
+    "revision": "07247b0c8fef0b8d712b",
+    "url": "./static/js/1.07247b0c.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "b20914ec5482543a0b1b2c6d5509ab96",
+    "url": "./static/media/dropdown-blue.b20914ec.svg"
+  },
+  {
+    "revision": "cd061363bbf9cd7a26cb09b642dcaf63",
+    "url": "./static/media/arrow-down-blue.cd061363.svg"
+  },
+  {
+    "revision": "c0dedd2f2ed0c4d07d7ca75af3f0a65f",
+    "url": "./static/media/arrow-down-grey.c0dedd2f.svg"
+  },
+  {
+    "revision": "b96d70e10dd30a64a0d122603577c8ae",
+    "url": "./static/media/dropup-blue.b96d70e1.svg"
+  },
+  {
+    "revision": "0be980e8395d5fde00ee3c8165d0c15a",
+    "url": "./static/media/trust-wallet-logo.0be980e8.svg"
+  },
+  {
+    "revision": "faebadbf95c3c3085587",
+    "url": "./static/js/main.faebadbf.chunk.js"
+  },
+  {
+    "revision": "023762b6aec2a2249b8fdfb638f00ef3",
+    "url": "./static/media/metamask-logo.023762b6.png"
+  },
+  {
+    "revision": "040d3851a083aab7f00a01aa0ddd023b",
+    "url": "./static/media/ethereum-logo.040d3851.png"
+  },
+  {
+    "revision": "674400972753804891ec372652944539",
+    "url": "./static/media/magnifying-glass.67440097.svg"
+  },
+  {
+    "revision": "c89be69c2593e3de93f102d510b814a9",
+    "url": "./static/media/qr-code.c89be69c.svg"
+  },
+  {
+    "revision": "e8021e51723d455b2f9fa2446808d2a3",
+    "url": "./static/media/plus-blue.e8021e51.svg"
+  },
+  {
+    "revision": "d8e0be7d6efb0b53c37eb75e44b35bda",
+    "url": "./static/media/plus-grey.d8e0be7d.svg"
+  },
+  {
+    "revision": "c73674bb0d585b4172cd0d8c15c5af34",
+    "url": "./static/media/generic-token-logo.c73674bb.png"
+  },
+  {
+    "revision": "7d32d2fa19d17d6ab9f0e0067bebaf96",
+    "url": "./static/media/dropdown.7d32d2fa.svg"
+  },
+  {
+    "revision": "b33790db34ea5596afb34800197562c2",
+    "url": "./index.html"
+  }
+];
